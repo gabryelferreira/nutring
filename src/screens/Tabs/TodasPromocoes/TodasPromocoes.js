3a02e227ea53ffb0472a7da8c2d9a2d5e151639f
Gabryel Ferreira <gabryel.ferreira@hotmail.com>
2019-05-16 07:52:41 -0300

    Iniciando promocoes

--- src/screens/Tabs/TodasPromocoes/TodasPromocoes.js
+++ src/screens/Tabs/TodasPromocoes/TodasPromocoes.js
@@ -19,73 +19,141 @@ PICTURE_MIN_HEIGHT = 50;
 
 export default class TodasPromocoes extends Network {
 
-  static navigationOptions = () => ({
-      title: 'Promoções',
-      headerBackTitle: "",
-      header: (
-          <View></View>
-      )
-  });
+    static navigationOptions = () => ({
+        title: 'Promoções',
+        headerBackTitle: "",
+        header: (
+            <View></View>
+        )
+    });
 
-  constructor(props){
-    super(props);
-    this.state = {
-        scrollY: new Animated.Value(0)
+    limit = 10;
+    promocoesDoDia = [];
+    promocoesDaSemana = [];
+
+
+    constructor(props){
+        super(props);
+        this.state = {
+            scrollY: new Animated.Value(0),
+            loadingInitial: true,
+            promocoesDoDia: [],
+            promocoesDaSemana: []
+        }
     }
-  }
 
-  componentDidMount(){
-    // this.getNotificacoes();
-  }
+    componentDidMount(){
+        // this.getNotificacoes();
+        this.getPromocoesDoDia();
+    }
 
-  render() {
+    async getPromocoesDoDia(){
+        let result = await this.callMethod('getPromocoesDoDia', { limit: this.limit, offset: 0 });
+        if (result.success){
+            this.promocoesDoDia = result.result;
+            this.getPromocoesDaSemana();
+        }
+    }
 
-    // const headerHeight = this.state.scrollY.interpolate({
-    //     inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
-    //     outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
-    //     extrapolate: 'clamp'
-    // });
+    async getPromocoesDaSemana(){
+        let result = await this.callMethod('getPromocoesDaSemana', { limit: this.limit, offset: 0 });
+        if (result.success){
+        this.promocoesDaSemana = result.result;
+        this.setState({
+            promocoesDoDia: this.promocoesDoDia,
+            promocoesDaSemana: this.promocoesDaSemana,
+            loadingInitial: false
+        });
+        }
+    }
 
-    // const headerColor = this.state.scrollY.interpolate({
-    //     inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
-    //     outputRange: ['rgb(0, 0, 0)', 'rgb(255, 255, 255)']
-    // });
+    renderPromocoesDoDia(){
+        return this.state.promocoesDoDia.map(promocao => {
+            return <CardTwo key={promocao.id_promocao.toString()} imagem={promocao.foto_promocao} nome={promocao.nome} descricao={promocao.descricao}/>
+        })
+    }
 
-    const pictureOpacity = this.state.scrollY.interpolate({
-        inputRange: [0, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT],
-        outputRange: [1, 0]
-    });
+    renderPromocoesDaSemana(){
+        return this.state.promocoesDaSemana.map(promocao => {
+            return <CardTwo key={promocao.id_promocao.toString()} imagem={promocao.foto_promocao} nome={promocao.nome} descricao={promocao.descricao}/>
+        })
+    }
 
-    const elevation = this.state.scrollY.interpolate({
-        inputRange: [0, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT - 0.01, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT],
-        outputRange: [0, 0, 1],
-        extrapolate: 'clamp'
-    });
+    renderFullLoader(){
+        return (
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 30}}>
+                <ActivityIndicator size="small" color="#777" />
+            </View>
+        );
+    }
+
+    renderContent(){
+        if (this.state.loadingInitial) return this.renderFullLoader();
+
+        return (
+            <>
+                <Text style={[styles.titulo, styles.paddingHorizontal]}>De Hoje</Text>
+                <ScrollView horizontal={true} contentContainerStyle={{paddingHorizontal: 15, paddingVertical: 10}} showsHorizontalScrollIndicator={false}>
+                    {this.renderPromocoesDoDia()}
+                </ScrollView>
+
+                <Text style={[styles.titulo, styles.paddingHorizontal]}>Da Semana</Text>
+                <ScrollView horizontal={true} contentContainerStyle={{paddingHorizontal: 15, paddingVertical: 10}} showsHorizontalScrollIndicator={false}>
+                    {this.renderPromocoesDaSemana()}
+                </ScrollView>
+
+                <Text style={[styles.titulo, styles.paddingHorizontal]}><Text style={{fontWeight: 'normal'}}>Restaurante</Text> Destaque</Text>
+                <View style={{flex: 1, paddingHorizontal: 15, paddingVertical: 10}}>
+                    <TouchableOpacity style={{flex: 1, borderRadius: 10, elevation: 1, overflow: 'hidden'}}>
+                        <Image resizeMethod="resize" style={{flex: 1, width: undefined, height: 200}} source={require('../../../assets/imgs/promocoes.jpg')}/>
+                        <View style={{paddingHorizontal: 15, paddingVertical: 10}}>
+                            <Text style={{fontSize: 18, fontWeight: 'bold', color: '#000'}}>Obba Gastronomia Saudável</Text>
+                            <Text style={{fontSize: 14, fontWeight: 'normal', color: '#000'}}>Parabéns por ser o restaurante destaque da semana!</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </>
+        );
+    }
+
+    render() {
+
+        const elevation = this.state.scrollY.interpolate({
+            inputRange: [0, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT - 0.01, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT],
+            outputRange: [0, 0, 1],
+            extrapolate: 'clamp'
+        });
 
 
-    const headerBackgroundColor = this.state.scrollY.interpolate({
-        inputRange: [0, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT - 0.01, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT],
-        outputRange: ['rgba(255, 255, 255, 0)', 'rgba(255, 255, 255, 0)', 'rgba(255, 255, 255, 1)']
-    });
+        const headerBackgroundColor = this.state.scrollY.interpolate({
+            inputRange: [0, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT - 0.01, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT],
+            outputRange: ['rgba(255, 255, 255, 0)', 'rgba(255, 255, 255, 0)', 'rgba(255, 255, 255, 1)']
+        });
 
-    const headerColorOpacity = this.state.scrollY.interpolate({
-        inputRange: [0, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT - 0.01, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT],
-        outputRange: [0, 0, 1]
-    });
+        const headerColorOpacity = this.state.scrollY.interpolate({
+            inputRange: [0, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT - 0.01, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT],
+            outputRange: [0, 0, 1]
+        });
 
-    const headerColor = this.state.scrollY.interpolate({
-        inputRange: [0, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT - 0.01, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT],
-        outputRange: ['#fff', '#fff', '#000']
-    });
-    
-    const textOpacity = this.state.scrollY.interpolate({
-        inputRange: [0, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT],
-        outputRange: [1, 0.2],
-        extrapolate: 'clamp'
-    });
+        const headerColor = this.state.scrollY.interpolate({
+            inputRange: [0, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT - 0.01, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT],
+            outputRange: ['#fff', '#fff', '#000']
+        });
+        
+        const textOpacity = this.state.scrollY.interpolate({
+            inputRange: [0, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT],
+            outputRange: [1, 0.2],
+            extrapolate: 'clamp'
+        });
 
-    const AnimatedIcon = Animated.createAnimatedComponent(Icon)
-    
+        const positionLeft = this.state.scrollY.interpolate({
+            inputRange: [0, PICTURE_MAX_HEIGHT - PICTURE_MIN_HEIGHT],
+            outputRange: [20, imageWidth/2 - 100],
+            extrapolate: 'clamp'
+        })
+
+        const AnimatedIcon = Animated.createAnimatedComponent(Icon)
+        
         return (
             <SafeAreaView style={{flex: 1}}>
                 <Animated.View style={{position: 'absolute', paddingHorizontal: 5, zIndex: 9999, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', left: 0, top: 0, right: 0, height: HEADER_HEIGHT, width: imageWidth, backgroundColor: headerBackgroundColor, elevation}}>
@@ -109,34 +177,7 @@ export default class TodasPromocoes extends Network {
                     )}>
                     <View style={{marginTop: PICTURE_MAX_HEIGHT, paddingBottom: 15, backgroundColor: '#fff'}}>
                         <Animated.Text style={{position: 'absolute', left: 20, top: -55, color: '#fff', fontSize: 30, fontWeight: 'bold', opacity: textOpacity}}>Promoções</Animated.Text>
-                        <Text style={[styles.titulo, styles.paddingHorizontal]}>De Hoje</Text>
-                        <ScrollView horizontal={true} contentContainerStyle={{paddingHorizontal: 15, paddingVertical: 10}} showsHorizontalScrollIndicator={false}>
-                            <CardTwo key={"restaurante1"} imagem={'https://img.itdg.com.br/tdg/images/blog/uploads/2017/01/shutterstock_283021049.jpg'} nome={"Let's Wok"} descricao={'10% de DESCONTO no filé de frango + acompanhamentos, SÓ HOJE'}/>
-                            <CardTwo key={"restaurante1"} imagem={'https://img.itdg.com.br/tdg/images/blog/uploads/2017/01/shutterstock_283021049.jpg'} nome={"Let's Wok"} descricao={'10% de DESCONTO no filé de frango + acompanhamentos, SÓ HOJE'}/>
-                            <CardTwo key={"restaurante1"} imagem={'https://img.itdg.com.br/tdg/images/blog/uploads/2017/01/shutterstock_283021049.jpg'} nome={"Let's Wok"} descricao={'10% de DESCONTO no filé de frango + acompanhamentos, SÓ HOJE'}/>
-                            <CardTwo key={"restaurante1"} imagem={'https://img.itdg.com.br/tdg/images/blog/uploads/2017/01/shutterstock_283021049.jpg'} nome={"Let's Wok"} descricao={'10% de DESCONTO no filé de frango + acompanhamentos, SÓ HOJE'}/>
-                            <CardTwo key={"restaurante1"} imagem={'https://img.itdg.com.br/tdg/images/blog/uploads/2017/01/shutterstock_283021049.jpg'} nome={"Let's Wok"} descricao={'10% de DESCONTO no filé de frango + acompanhamentos, SÓ HOJE'}/>
-                        </ScrollView>
-
-                        <Text style={[styles.titulo, styles.paddingHorizontal]}>Da Semana</Text>
-                        <ScrollView horizontal={true} contentContainerStyle={{paddingHorizontal: 15, paddingVertical: 10}} showsHorizontalScrollIndicator={false}>
-                            <CardTwo key={"restaurante1"} imagem={'https://img.itdg.com.br/tdg/images/blog/uploads/2017/01/shutterstock_283021049.jpg'} nome={"Let's Wok"} descricao={'10% de DESCONTO no filé de frango + acompanhamentos, SÓ HOJE'}/>
-                            <CardTwo key={"restaurante1"} imagem={'https://img.itdg.com.br/tdg/images/blog/uploads/2017/01/shutterstock_283021049.jpg'} nome={"Let's Wok"} descricao={'10% de DESCONTO no filé de frango + acompanhamentos, SÓ HOJE'}/>
-                            <CardTwo key={"restaurante1"} imagem={'https://img.itdg.com.br/tdg/images/blog/uploads/2017/01/shutterstock_283021049.jpg'} nome={"Let's Wok"} descricao={'10% de DESCONTO no filé de frango + acompanhamentos, SÓ HOJE'}/>
-                            <CardTwo key={"restaurante1"} imagem={'https://img.itdg.com.br/tdg/images/blog/uploads/2017/01/shutterstock_283021049.jpg'} nome={"Let's Wok"} descricao={'10% de DESCONTO no filé de frango + acompanhamentos, SÓ HOJE'}/>
-                            <CardTwo key={"restaurante1"} imagem={'https://img.itdg.com.br/tdg/images/blog/uploads/2017/01/shutterstock_283021049.jpg'} nome={"Let's Wok"} descricao={'10% de DESCONTO no filé de frango + acompanhamentos, SÓ HOJE'}/>
-                        </ScrollView>
-
-                        <Text style={[styles.titulo, styles.paddingHorizontal]}><Text style={{fontWeight: 'normal'}}>Restaurante</Text> Destaque</Text>
-                        <View style={{flex: 1, paddingHorizontal: 15, paddingVertical: 10}}>
-                            <TouchableOpacity style={{flex: 1, borderRadius: 10, elevation: 1, overflow: 'hidden'}}>
-                                <Image resizeMethod="resize" style={{flex: 1, width: undefined, height: 200}} source={require('../../../assets/imgs/promocoes.jpg')}/>
-                                <View style={{paddingHorizontal: 15, paddingVertical: 10}}>
-                                    <Text style={{fontSize: 18, fontWeight: 'bold', color: '#000'}}>Obba Gastronomia Saudável</Text>
-                                    <Text style={{fontSize: 14, fontWeight: 'normal', color: '#000'}}>Parabéns por ser o restaurante destaque da semana!</Text>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
+                        {this.renderContent()}
 
 
                     </View>
@@ -146,7 +187,7 @@ export default class TodasPromocoes extends Network {
             </SafeAreaView> 
         );
 
-  }
+    }
 
 
 }
